f3ba023d4fd52133d65d1f3847a23a67ea57fbc1
Paulo Henrique <39860359+PauloHenrique1993@users.noreply.github.com>
2021-09-13 16:50:16 -0300

    Update validando_email.sql

--- validando_email.sql
+++ validando_email.sql
@@ -1,12 +1,18 @@
 DECLARE @Teste TABLE ( [Email] VARCHAR(200) )
  
-INSERT INTO @Teste 
+INSERT INTO 
+	@Teste 
 VALUES 
 	('paulo@g.com'), ('PauloHenrique'), ('Paulo@'), ('@'), ('@.com'), ('paulo@dominio.com.br'), ('email@gmail.com'), 
 		('email@gmail'), ('email@dominio.')
  
-SELECT *
-FROM @Teste
-WHERE [Email] NOT LIKE '%[^a-z,0-9,@,.-_]%' 
-AND [Email] LIKE '%_@_%_.__%' 
-AND [Email] NOT LIKE '%_@@_%_.__%'
+SELECT 
+	*
+FROM 
+	@Teste
+WHERE 
+	[Email] NOT LIKE '%[^a-z,0-9,@,.-_]%' 
+AND 
+	[Email] LIKE '%_@_%_.__%' 
+AND 
+	[Email] NOT LIKE '%_@@_%_.__%'
