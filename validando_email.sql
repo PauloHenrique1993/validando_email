1d33f274811f6743e93069c5283f17c3e144a67a
Paulo Henrique <39860359+PauloHenrique1993@users.noreply.github.com>
2021-09-13 17:43:03 -0300

    adicionando e-mail

--- validando_email.sql
+++ validando_email.sql
@@ -4,15 +4,15 @@ INSERT INTO
 	@Teste 
 VALUES 
 	('paulo@g.com'), ('PauloHenrique'), ('Paulo@'), ('@'), ('@.com'), ('paulo@dominio.com.br'), ('email@gmail.com'), 
-		('email@gmail'), ('email@dominio.')
+		('email@gmail'),('email@gmail.') ,('email@dominio.'),('email@dominio,com'),('paulo@outlook.com'),('paulo@@yahoo.com'),('paulo$@yahoo.com')
  
 SELECT 
 	*
 FROM 
 	@Teste
 WHERE 
-	[Email] NOT LIKE '%[^a-z,0-9,@,.-_]%' 
-AND 
+	[Email] NOT LIKE '%[^a-z,0-9,@,.-_]%'
+AND
 	[Email] LIKE '%_@_%_.__%' 
 AND 
 	[Email] NOT LIKE '%_@@_%_.__%'
